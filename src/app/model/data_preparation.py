98fd0f04e2ccd0a714477424f1a440b6962063b1
Ricardo <ricardoataidejunior@gmail.com>
2026-03-05 13:08:58 -0300

    feat/refactor: add model salving to notebook - app.model definitions

--- src/app/model/data_preparation.py
+++ src/app/model/data_preparation.py
@@ -1,17 +1,27 @@
 import pandas as pd 
-
+from sklearn import joblib 
+import os 
 
 class DataPreparation:
     def __init__(self,
-                 dataset):
+                 dataset,
+                 encoders_path = './encoders/',
+                 model_path = './model_definition/'):
+        
 
         self.expected_columns = ['']
         self.dataset = dataset 
-
+        self.encoders_path = encoders_path
+        self.model_path = model_path
+        
+    
+    def _encode_features(self):
+        ...
     def check_dataset_structure(self):
         
-        if self.dataset.columns
-    df_3 = pd.read_csv('./data/df_3.csv')
-    df_3['faixa_etaria'] = df_3.Age.apply(lambda x: 10*(x//10))
-    df_3['entre_30_50'] = df_3.Age.apply(lambda x: 1 if (x>=30) and (x<=50) else 0)
-    df_3['Annual_Premium_br'] = df_3.Annual_Premium.apply(lambda x: x * 0.057)
+        if self.dataset.columns:
+            ...
+
+    def add_features(self):
+        self.dataset['faixa_etaria'] = self.dataset.Age.apply(lambda x: 10*(x//10))
+        self.dataset['entre_30_50'] = self.dataset.Age.apply(lambda x: 1 if (x>=30) and (x<=50) else 0)
