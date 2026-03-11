6b20411a0cfbe382a40edd59a82d96086e400d9f
rycardyo <ricardoataidejunior@gmail.com>
2026-03-11 08:08:29 -0300

    feat: back_to_the_last_work

--- src/app/src/home.py
+++ src/app/src/home.py
@@ -3,6 +3,9 @@ import pandas as pd
 import json
 import os 
 from datetime import datetime
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).resolve().parents[1]
 
 def check_columns(df):
     with open('../model/resources/dataset/.required_columns.json', 'r') as f:
@@ -17,31 +20,62 @@ def check_columns(df):
 
     return True
 
+def get_last_action_path(actions_path : str = 'data/actions'):
+    ''' 
+        Verify if exists some file in 'data/actions
+    '''
 
-uploaded_file = st.file_uploader('Upload a customers list', type = ['csv', 'xlsx'])
+    files = os.listdir(os.path.join(ROOT_PATH, actions_path))
+    csv_files = []
+    for f in files:
+        if f.endswith('.csv'):
+            csv_files.append(os.path.join(ROOT_PATH, actions_path, f))
+    
+    if csv_files != []:
+        return csv_files[-1]
+    
+    return False
 
-if uploaded_file:
-    if uploaded_file.name.endswith('.csv'):
-        df = pd.read_csv(uploaded_file)
-    else:
-        df = pd.read_excel(uploaded_file)
+col_left, btn1, btn2, col_right = st.columns([2,1,1,2])
 
-    if check_columns(df):
-        st.dataframe(df.sample(frac=.7))
+with btn1:
+    back_to_the_last = st.button(label = "Retomar ultimo Trabalho")
+    if back_to_the_last:
+        if get_last_action_path():
+            st.switch_page('pages/1_model_predictions.py', 
+                                query_params={'datasetFPath' : get_last_action_path(),
+                                            'action' : 'last_action'})
 
-make_predictions = st.button('Make Predictions')
+        else:
+            st.error('Não ha trabalhos registrados')
 
-if make_predictions:
-    try:
-        df.fillna(value = '', inplace=True)
-        attatchedFName = datetime.now().strftime('%d_%m_%Y %H-%M:-%s')
-        attatchedFPath = f'./payload_items/{attatchedFName}.csv'
-        json_df = df.to_csv(attatchedFPath) 
-        
-        st.switch_page('pages/1_model_predictions.py', 
-                    query_params={'datasetFName' : attatchedFPath})
-        
-    except NameError:
-        st.warning('Para realizar predições, é necessário que um arquivo seja enviado.Por favor, anexe um arquivo e tente novamente')
+with btn2: 
+    uploaded_file = st.file_uploader('Upload a customers list', type = ['csv', 'xlsx'])
+    make_predictions = st.button('Make Predictions')
+
+
+    if uploaded_file:
+        if uploaded_file.name.endswith('.csv'):
+            df = pd.read_csv(uploaded_file)
+        else:
+            df = pd.read_excel(uploaded_file)
+
+        if check_columns(df):
+            st.dataframe(df.sample(frac=.7))
+
+
+    if make_predictions:
+        try:
+            df.fillna(value = '', inplace=True)
+            attatchedFName = datetime.now().strftime('%d_%m_%Y %H-%M:-%s')
+            attatchedFPath = f'./payload_items/{attatchedFName}.csv'
+            json_df = df.to_csv(attatchedFPath) 
+            
+            st.switch_page('pages/1_model_predictions.py', 
+                        query_params={'datasetFPath' : attatchedFPath,
+                                      'action'  : 'inference'})
+            
+        except NameError:
+            st.warning('Para realizar predições, é necessário que um arquivo seja enviado.Por favor, anexe um arquivo e tente novamente')
 
     
--- src/app/src/pages/1_model_predictions.py
+++ src/app/src/pages/1_model_predictions.py
@@ -12,17 +12,64 @@ from data_preparation  import DataPreparation
 
 
 st.title('Ranked List')
-
 st.markdown(''' Lista de clientes rankeados por propensão de compra (%)''')
 
 
-dataset = pd.read_csv(st.query_params['datasetFName']).drop(columns=['Unnamed: 0', 'unnamed: 0','id','response'])
-data_preparator = DataPreparation(dataset, verbose = True)
-dataset_to_model = data_preparator.pipeline()
+def get_last_action_dataset():
+    dataset = pd.read_csv(st.query_params['datasetFPath'])
+    return dataset 
 
-model = RankerModel()
-propensity_to_buy = model.predict(dataset_to_model)
 
-dataset['Propensity_to_buy'] = propensity_to_buy * 100
-dataset['Propensity_to_buy'] = dataset['Propensity_to_buy'].apply(lambda x: round(x,2))
-st.dataframe(dataset.sort_values(by = 'Propensity_to_buy', ascending = False))
+def get_inference_dataset():
+    dataset = pd.read_csv(st.query_params['datasetFPath']).drop(columns=['Unnamed: 0', 'unnamed: 0','id','response'])
+    data_preparator = DataPreparation(dataset, verbose = True)
+    dataset_to_model = data_preparator.pipeline()
+
+    model = RankerModel()
+    propensity_to_buy = model.predict(dataset_to_model)
+
+    dataset['Propensity_to_buy'] = propensity_to_buy * 100
+    dataset['Propensity_to_buy'] = dataset['Propensity_to_buy'].apply(lambda x: round(x,2))
+    dataset = dataset.sort_values(by = 'Propensity_to_buy', ascending = False)
+    dataset["Status"] = "Pendente"
+
+    return dataset 
+
+
+def get_dataset(action):
+    if action == 'inference':
+        return get_inference_dataset()
+    
+    if action == 'last_action':
+        return get_last_action_dataset()
+    
+    raise ValueError('Nao foi possivel retornar o dataset, os parametros permitidos são [last_action, inference]')
+
+
+dataset = get_dataset(st.query_params['action'])
+
+columns_disabled = [x for x in dataset.columns if x != "Status"]
+dataset_result   = st.data_editor(
+    dataset, 
+    column_config = {
+                        "Status" : st.column_config.SelectboxColumn(
+                            "Resultado da proposta",
+                            help = "Insira aqui o resultado do contato feito com o cliente",
+                            options=[
+                                "🫀 Aceita",
+                                "Aceita com desconto",
+                                "Pendente",
+                                "Rejeitada"
+                            ],
+                            required=True,
+                        )
+            },
+    disabled = columns_disabled,
+    hide_index=True
+    )
+
+
+salvar_resultado = st.button(label = "Save Results")
+if salvar_resultado:
+    dataset_result.to_csv(os.path.join(root_path, 'data/results.csv'))
+
